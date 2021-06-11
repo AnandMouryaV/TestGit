ff27f90a69719e8d309ae5eb91eda209c4b5944a
AnandMourya <themourya.v@gmail.com>
2021-06-11 23:57:08 +0530

    Added the changes for Observer Pattern

--- src/com/CreationalDesignPatterns/ObserverPattern/ObserverPatterDemo.java
+++ src/com/CreationalDesignPatterns/ObserverPattern/ObserverPatterDemo.java
@@ -3,7 +3,7 @@ package com.CreationalDesignPatterns.ObserverPattern;
 public class ObserverPatterDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Observer Pattern changes  to be added");
 
 	}
 
