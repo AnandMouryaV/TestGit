19fa7e48e6e7c1c90bea6afe6768463cf72cfda4
AnandMourya <themourya.v@gmail.com>
2021-06-11 23:49:46 +0530

    Added Observer Pattern

---
+++ src/com/CreationalDesignPatterns/ObserverPattern/ObserverPatterDemo.java
@@ -0,0 +1,10 @@
+package com.CreationalDesignPatterns.ObserverPattern;
+
+public class ObserverPatterDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
